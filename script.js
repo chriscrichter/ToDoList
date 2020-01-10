b4004d7808db9b2562b545627540d06c975b7dd5
Ellen Park <ellenpark781@gmail.com>
2020-01-10 12:38:26 -0500

    putting new input

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
+//our todos
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
 renderTodos();
-
+//Shows what todos we already have
 function renderTodos() {
   // Clear todoList element and update todoCountSpan
   todoList.innerHTML = "";
@@ -21,3 +22,32 @@ function renderTodos() {
     todoList.appendChild(li);
   }
 }
+
+function newElement() {
+  
+//todoInput.value = Our input
+// add our input to the todo list 
+
+}
+//update todo 
+// * Add an event listener so that when a user hits enter, the value from the todo input field is pushed to our todo array.
+
+// * Make sure that empty values are not pushed to the array.
+
+// * Once the value has been added to the array, clear the input field and re-render the todo list.
+todoForm.addEventListener("submit", function(event){
+  event.preventDefault(); 
+
+  console.log (todoInput.value);
+
+
+var todoText = todoInput.value.trim();
+if (todoText === "") {
+return;
+}
+todos.push(todoText);
+todoInput.value = ""; 
+
+renderTodos(); 
+
+});
